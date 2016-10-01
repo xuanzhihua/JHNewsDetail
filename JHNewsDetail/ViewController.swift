8883ed779ab2fd89abaf0d2ec3b39b1059bf0a6d
Air <a123@123demacbook-pro.local>
2016-10-01 13:45:09 +0800

    Initial Commit

---
+++ JHNewsDetail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JHNewsDetail
+//
+//  Created by 123 on 16/10/1.
+//  Copyright © 2016年 叶建华. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
